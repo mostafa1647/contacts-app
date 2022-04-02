7cddd2164e2a849d8b9b6b12df8c776d76231d15
Mostafa Mojtahedi <mojtahedimostafa.m@gmail.com>
2022-04-03 00:28:11 +0430

    completed header

--- frontend/src/components/Header/Header.tsx
+++ frontend/src/components/Header/Header.tsx
@@ -1,11 +1,19 @@
 import type { FC } from "react";
+import Container from "../Container";
+import styleClasses from "./Header.module.scss";
+import { Link } from "react-router-dom";
+import { ReactComponent as LogoSvg } from "../../assets/images/logo-on-dark-bg.svg";
 
-const Header: FC = (): JSX.Element => {
-	return (
-		<>
-			<p>header</p>
-		</>
-	);
-};
+const Header: FC = (): JSX.Element => (
+	<Container>
+		<div className={styleClasses.header}>
+			<div className={styleClasses.logo}>
+				<Link className={styleClasses.logoLink} to={"/"}>
+					<LogoSvg width={180} />
+				</Link>
+			</div>
+		</div>
+	</Container>
+);
 
 export default Header;
