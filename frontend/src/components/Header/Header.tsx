cb3ba4a09849d52c236c1e759962fe326dbcd1c4
Mostafa Mojtahedi <mojtahedimostafa.m@gmail.com>
2022-04-03 14:42:15 +0430

    changed logo

--- frontend/src/components/Header/Header.tsx
+++ frontend/src/components/Header/Header.tsx
@@ -2,14 +2,15 @@ import type { FC } from "react";
 import Container from "../Container";
 import styleClasses from "./Header.module.scss";
 import { Link } from "react-router-dom";
-import { ReactComponent as LogoSvg } from "../../assets/images/logo-on-dark-bg.svg";
+import { ReactComponent as ReactLogo } from "../../assets/images/react-logo.svg";
 
 const Header: FC = (): JSX.Element => (
 	<Container>
 		<div className={styleClasses.header}>
 			<div className={styleClasses.logo}>
 				<Link className={styleClasses.logoLink} to={"/"}>
-					<LogoSvg width={180} />
+					<ReactLogo className={styleClasses.logoImage} />
+					<span className={styleClasses.logoText}>React</span>
 				</Link>
 			</div>
 		</div>
