7a0bfc66c3d30506b065c37a0bea767b91740b1a
Mostafa Mojtahedi <mojtahedimostafa.m@gmail.com>
2022-04-03 18:24:00 +0430

    added api utils

---
+++ frontend/src/utils/api/dto/contacts.dto.ts
@@ -0,0 +1,35 @@
+export enum GenderEnum {
+	Female = "Female",
+	Male = "Male",
+	NonBinary = "Non-binary",
+	Agender = "Agender",
+	Bigender = "Bigender",
+	Genderqueer = "Genderqueer",
+	Genderfluid = "Genderfluid",
+	Polygender = "Polygender",
+}
+
+export interface ContactDto {
+	name: string;
+	email: string;
+	gender?: GenderEnum | null | undefined;
+	phone: string;
+	note?: string | null | undefined;
+	telegram?: string | null | undefined;
+	avatar: string;
+	company?: string | null | undefined;
+	address?: string | null | undefined;
+	createdAt?: string | number | Date | null | undefined;
+	updatedAt?: string | number | Date | null | undefined;
+	id: number;
+}
+
+export interface ContactsListDto {
+	meta: {
+		skipped?: number;
+		limit?: number;
+		total?: number;
+		criteria?: any;
+	};
+	items: ContactDto[];
+}
