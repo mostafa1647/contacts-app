cafaf68303110cd08318c99cc5ee77dafd01e572
Mostafa Mojtahedi <mojtahedimostafa.m@gmail.com>
2022-04-05 12:21:22 +0430

    type refactor

--- frontend/src/pages/HomePage/HomePage.tsx
+++ frontend/src/pages/HomePage/HomePage.tsx
@@ -1,8 +1,10 @@
+import { FC, ReactNode } from "react";
+
 interface HomePageProps {
 	showSearchResults: boolean;
 }
 
-const HomePage = ({ showSearchResults }: HomePageProps): JSX.Element => {
+const HomePage: FC<HomePageProps> = ({ showSearchResults }): JSX.Element => {
 	return (
 		<>
 			<p>HomePage {showSearchResults && "search"}</p>
