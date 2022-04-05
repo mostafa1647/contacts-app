65a52ad2341d8423db93c7fae9101018664978cd
Mostafa Mojtahedi <mojtahedimostafa.m@gmail.com>
2022-04-05 10:51:35 +0430

    added notFound page
    there is a bug in ant.design button, added todo comment

--- frontend/src/pages/NotFound/NotFound.tsx
+++ frontend/src/pages/NotFound/NotFound.tsx
@@ -1,10 +1,24 @@
 import type { FC } from "react";
+import { useNavigate } from "react-router-dom";
+import { Result, Button } from "antd";
 
 const NotFound: FC = (): JSX.Element => {
+	const navigate = useNavigate();
+
 	return (
-		<>
-			<p>not found</p>
-		</>
+		<Result
+			status="404"
+			title="404"
+			subTitle="Sorry, the page you visited does not exist."
+			extra={
+				// TODO: button in ant.design shows spin in ts, check why is that and fix it
+				<Button
+					type="primary"
+					onClick={() => navigate("/", { replace: true })}>
+					Back Home
+				</Button>
+			}
+		/>
 	);
 };
 
