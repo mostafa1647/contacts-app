107977bef9673dba91f892c98dc8ee6eba9ab675
Mostafa Mojtahedi <mojtahedimostafa.m@gmail.com>
2022-04-05 13:28:22 +0430

    configured react query provider

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,20 @@
 import React, { FC } from "react";
 import { useRoutes } from "react-router-dom";
 import { routes } from "./utils/routes/routes";
+import { QueryClient, QueryClientProvider } from "react-query";
+
+const queryClient = new QueryClient();
 
 const App: FC = (): JSX.Element => {
 	const element = useRoutes(routes);
-	return <div className="App">{element}</div>;
+
+	return (
+		<div className="App">
+			<QueryClientProvider client={queryClient}>
+				{element}
+			</QueryClientProvider>
+		</div>
+	);
 };
 
 export default App;
