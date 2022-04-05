a4001d933234918ebcfe59051fcb71ed0a573cbe
Mostafa Mojtahedi <mojtahedimostafa.m@gmail.com>
2022-04-05 12:21:44 +0430

    added Error Component

---
+++ frontend/src/components/Error/Error.tsx
@@ -0,0 +1,20 @@
+import type { FC, ReactNode } from "react";
+import { Result, Space } from "antd";
+
+interface ErrorProps {
+	title: string;
+	message: string;
+}
+
+const Error: FC<ErrorProps> = ({ title, message }): JSX.Element => (
+	<Space
+		direction={"vertical"}
+		size={"large"}
+		style={{
+			width: "100%",
+		}}>
+		<Result status="error" title={title} subTitle={message}></Result>
+	</Space>
+);
+
+export default Error;
