edfd1d1a5c80e68d203a0eb1e28de6a323a6977f
Mostafa Mojtahedi <mojtahedimostafa.m@gmail.com>
2022-04-08 15:18:24 +0430

    downgrade react version to 17.0.2 cause of ant.design incompatibility with react@18

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,20 +1,33 @@
 import React from "react";
-import { createRoot } from "react-dom/client";
+// import { createRoot } from "react-dom/client";
+import ReactDOM from "react-dom";
 import App from "./App";
 import "antd/dist/antd.less";
 import "./assets/styles/index.scss";
 import { BrowserRouter } from "react-router-dom";
 
-const containerElement = document.getElementById("root") as Element;
-const root = createRoot(containerElement);
-
 /**
- * can not use React strict mode cause of antd
- * antd version "^4.19.5"
- * React version "^18.0.0"
+ * it seems ant.design is planning to support react@18 in antd@5,
+ * so we downgrade to react@17.0.2
+ * @see https://github.com/ant-design/ant-design/issues/26136
+ * @see https://github.com/ant-design/ant-design/issues/33862
  */
-root.render(
-	<BrowserRouter>
-		<App />
-	</BrowserRouter>
+
+// const containerElement = document.getElementById("root") as Element;
+// const root = createRoot(containerElement);
+// root.render(
+// 	<React.StrictMode>
+// 		<BrowserRouter>
+// 			<App />
+// 		</BrowserRouter>
+// 	</React.StrictMode>
+// );
+
+ReactDOM.render(
+	<React.StrictMode>
+		<BrowserRouter>
+			<App />
+		</BrowserRouter>
+	</React.StrictMode>,
+	document.getElementById("root")
 );
