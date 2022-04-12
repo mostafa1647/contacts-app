7dd0dba7deb665ea1e602b411edd68a668321acb
Mostafa Mojtahedi <mojtahedimostafa.m@gmail.com>
2022-04-12 14:24:53 +0430

    added contacts module

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from "@nestjs/common";
 import { AppController } from "./app.controller";
 import { AppService } from "./app.service";
+import { ContactsModule } from './contacts/contacts.module';
 
 @Module({
-	imports: [],
+	imports: [ContactsModule],
 	controllers: [AppController],
 	providers: [AppService],
 })
